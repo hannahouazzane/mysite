390e0552135259f5fab5e06e11ff42a4e0128609
hannahouazzane <hannahouazzane@gmail.com>
2022-06-24 11:27:34 +0100

    added wagtail files and first experiment

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ab5k!x7t!8#cx2k))4v=))s30#cs$%1-w17k-rd^zwo0)yl2&h"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
